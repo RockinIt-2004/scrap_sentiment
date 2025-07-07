c5b1727465c59dbb82ece281c1b6fc0898080f97
Dhruv <dhruv.2125cs1182@kiet.edu>
2025-07-07 16:51:42 +0530

    h

--- app.py
+++ app.py
@@ -30,5 +30,7 @@ def results():
                            results=analyzed,
                            counts=counts)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
